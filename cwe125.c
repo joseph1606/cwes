bf0b37e245e9bf5d4e2602a457f13faaa413c7e7
joseph1606 <108588134+joseph1606@users.noreply.github.com>
2023-06-26 17:37:07 -0400

    Update cwe125.c

--- cwe125.c
+++ cwe125.c
@@ -1,23 +1,38 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int getValueFromArray(int *array, int len, int index);
 
 int main() {
-    int numbers[] = {1, 2, 3, 4, 5};
-    int *array = numbers;
-    int len = 5;
+    int len;
     int index;
     int value;
+    int i;
+    int numbers;
+
+    printf("Enter the length of the array: ");
+    scanf("%d", &len);
+
+    numbers = (int *)malloc(len * sizeof(int));
+    if (numbers == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    printf("Enter the elements of the array:\n");
+    for (i = 0; i < len; i++) {
+        printf("Enter element %d: ", i);
+        scanf("%d", &numbers[i]);
+    }
 
     printf("Enter the index: ");
     scanf("%d", &index);
 
-    value = getValueFromArray(array, len, index);
+    value = getValueFromArray(numbers, len, index);
 
     printf("Value is: %d\n", value);
 
+    free(numbers);
 
     return 0;
 }
@@ -25,7 +40,7 @@ int main() {
 int getValueFromArray(int *array, int len, int index) {
     int value;
 
-    if (index < len) {
+    if ( index < len) {
         value = array[index];
     } else {
         printf("Invalid index.\n");
@@ -34,4 +49,3 @@ int getValueFromArray(int *array, int len, int index) {
 
     return value;
 }
-
